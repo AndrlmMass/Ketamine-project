9a8ba14223b748ebfc2534d5f9cffd79a1469912
Andreas Lie Massey <107505110+AndrlmMass@users.noreply.github.com>
2023-01-17 16:23:59 +0100

    Added analysis files

---
+++ DataAnalysis/DataQuestAnalysis.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_excel('C:\\Users\\andre\\OneDrive\\Documents\\UiO\\Other_projects\\KetamineProject\\DataCollection\\QuestionnaireData\\QuestionnaireScores.xlsx')
+
+df.plot()
+
+
+
+
